a11dcca5054ab9df04945331f5dadc8207b8ad2c
Intesar-Tahmid <intesar3006@gmail.com>
2022-08-21 10:52:06 +0600

    Added email reporting through implicit intent

--- app/src/main/java/com/tomal66/cconnect/LoginActivity.kt
+++ app/src/main/java/com/tomal66/cconnect/LoginActivity.kt
@@ -1,14 +1,17 @@
 package com.tomal66.cconnect
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginBtn : Button
     private lateinit var signUpBtn : TextView
+    private lateinit var reportBtn : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,7 @@ class LoginActivity : AppCompatActivity() {
 
         loginBtn = findViewById(R.id.loginBtn)
         signUpBtn = findViewById(R.id.signUpBtn)
+        reportBtn = findViewById(R.id.reportBtn)
 
         loginBtn.setOnClickListener(){
             val intent = Intent(this, MainActivity::class.java)
@@ -26,5 +30,39 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+
+        reportBtn.setOnClickListener(){
+            sendEmail("intesar3006@gmail.com", "Application bug found by user","")
+        }
     }
+
+    private fun sendEmail(recipient: String, subject: String, message: String) {
+        /*ACTION_SEND action to launch an email client installed on your Android device.*/
+        val mIntent = Intent(Intent.ACTION_SEND)
+        /*To send an email you need to specify mailto: as URI using setData() method
+        and data type will be to text/plain using setType() method*/
+        mIntent.data = Uri.parse("mailto:")
+        mIntent.type = "text/plain"
+        // put recipient email in intent
+        /* recipient is put as array because you may wanna send email to multiple emails
+           so enter comma(,) separated emails, it will be stored in array*/
+        mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+        //put the Subject in the intent
+        mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        //put the message in the intent
+        mIntent.putExtra(Intent.EXTRA_TEXT, message)
+
+
+        try {
+            //start email intent
+            startActivity(Intent.createChooser(mIntent, "Choose Email Client..."))
+        }
+        catch (e: Exception){
+            //if any thing goes wrong for example no email client application or any exception
+            //get and show exception message
+            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+        }
+
+    }
+
 }
