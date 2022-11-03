043d121c7bd736dae4578b2dddc44d554ec77714
tomal66 <2019-717-841@student.cse.du.ac.bd>
2022-11-04 00:13:03 +0600

    Added Progress Dialogs

--- app/src/main/java/com/tomal66/cconnect/Activities/LoginActivity.kt
+++ app/src/main/java/com/tomal66/cconnect/Activities/LoginActivity.kt
@@ -78,7 +78,7 @@ class LoginActivity : AppCompatActivity() {
                         }
                     }
             }
-            dialog.dismiss()
+//            dialog.dismiss()
 
         }
 
